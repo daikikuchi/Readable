f40dbcbe93ca97eeccae644c3b6990664f973def
daikikuchi <kikuchi.dai@gmail.com>
2017-08-27 11:58:30 +1200

    Update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostsIndex from './components/posts_index';
 import PostsCategory from './components/posts_category';
 import PostDetail from './components/post_detail';
 import PostsForm from  './components/posts_form';
+import CommentsForm from  './components/posts_form';
 
 
 
@@ -15,13 +16,14 @@ class App extends Component {
         <Link className="link-to-index" to="/"><h1>Readable</h1></Link>
   </div>
       <div className="container text-center">
-      <Switch>
-        <Route exact path="/" render={() => <PostsIndex/>}/>
-        <Route exact path= "/posts/new" component={PostsForm}/>
-        <Route exact path="/:category/posts" component={PostsCategory}/>
-        <Route exact path = "/post/:id/edit" component={PostsForm}/>
-        <Route exact path = "/:category/:id" component={PostDetail}/>
-      </Switch>
+        <Switch>
+         <Route exact path="/" render={() => <PostsIndex/>}/>
+         <Route exact path= "/posts/new" component={PostsForm}/>
+         <Route exact path="/edit/comment/:id" component={CommentsForm}/>
+         <Route exact path="/post/:id/edit" component={PostsForm}/>
+  	     <Route exact path="/:category/posts" component={PostsCategory}/>
+         <Route exact path="/:category/:id" component={PostDetail}/>
+  </Switch>
       </div>
 </div>
     );
